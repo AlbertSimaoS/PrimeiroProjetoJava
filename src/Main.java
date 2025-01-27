5c2cbf4cd91d5c08619d6fa9bdeed53189463355
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-27 16:26:32 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Esse é o screen Match!");
+        System.out.println("Filme: Top Gun: Marverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8) /3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = """
+            Filme Top Gun
+            Filme de aventura
+            """;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
     }
 }
